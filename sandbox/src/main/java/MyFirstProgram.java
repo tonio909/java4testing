55d92c6ab2570e5b3298eb761a95894935740efe
tonio909 <anton.v.alekseev@yandex.ru>
2017-05-12 16:51:38 +0300

    Some functions added

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,8 +1,22 @@
 public class MyFirstProgram {
-	
-  public static void main (String[] args) {
 
-System.out.println("Hello, world!");
-  }
+    public static void main(String[] args) {
+
+        hello("world");
+
+        double a = 3254.03;
+        double b = 555.67;
+
+        System.out.println("Площадь прямоугольника cо сторонами " + a + " и " + b + " = " + square(a, b));
+    }
+
+    public static void hello(String w) {
+        System.out.println("Hello," + w + "!");
+    }
+
+    public static double square (double a, double b) {
+
+        return a * b;
+    }
 
 }
