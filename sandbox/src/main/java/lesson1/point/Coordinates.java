872fbb3d23120f97098e91a68c8ebcb536805fda
tonio909 <anton.v.alekseev@yandex.ru>
2017-05-12 21:52:54 +0300

    Task 2 commit

---
+++ sandbox/src/main/java/lesson1/point/Coordinates.java
@@ -0,0 +1,17 @@
+package lesson1.point;
+
+public class Coordinates {
+
+    double x1;
+    double y1;
+    double x2;
+    double y2;
+
+    public Coordinates(double x1, double y1, double x2, double y2) {
+
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+}
--- sandbox/src/main/java/lesson1/point/Point.java
+++ sandbox/src/main/java/lesson1/point/Point.java
@@ -4,6 +4,16 @@ public class Point {
 
     public static void main(String[] args) {
 
-        System.out.println("test");
+        Coordinates co = new Coordinates(4, 2, 6, 3);
+
+        System.out.println("Расстояние между точками A (" + co.x1 + ", " + co.y1 + ") и B (" +co.x2 + ", " + co.y2 + ") составляет " + distance(co));
+
     }
+
+    public static double distance(Coordinates co) {
+
+        return Math.sqrt( Math.pow(co.x2 - co.x1, 2) + Math.pow(co.y2 - co.y1, 2) );
+
+    }
+
 }
