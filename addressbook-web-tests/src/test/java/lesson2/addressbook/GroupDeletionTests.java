9ff1e40d0c3271c7c0d5467f067e49251c19097d
tonio909 <anton.v.alekseev@yandex.ru>
2017-05-18 21:46:40 +0300

    GroupDeletionTests added

---
+++ addressbook-web-tests/src/test/java/lesson2/addressbook/GroupDeletionTests.java
@@ -0,0 +1,16 @@
+package lesson2.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase {
+
+
+    @Test
+    public void testGroupDeletion() {
+        gotoGroupPage();
+        selectGroup();
+        deleteSelectedGroups();
+        returnToGroupPage();
+    }
+
+}
