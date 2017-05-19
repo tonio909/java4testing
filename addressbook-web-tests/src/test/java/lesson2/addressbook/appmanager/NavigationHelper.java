a7a67ff87e1776d7a8d392b71adeb6488ec311e5
tonio909 <anton.v.alekseev@yandex.ru>
2017-05-19 16:20:13 +0300

    HelperBase added

--- addressbook-web-tests/src/test/java/lesson2/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/lesson2/addressbook/appmanager/NavigationHelper.java
@@ -3,19 +3,17 @@ package lesson2.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class NavigationHelper {
-    private FirefoxDriver wd;
-
+public class NavigationHelper extends HelperBase{
 
     public NavigationHelper(FirefoxDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void gotoGroupPage() {
-        wd.findElement(By.linkText("groups")).click();
+        click(By.linkText("groups"));
     }
 
     public void gotoContactPage() {
-        wd.findElement(By.linkText("add new")).click();
+        click(By.linkText("add new"));
     }
 }
