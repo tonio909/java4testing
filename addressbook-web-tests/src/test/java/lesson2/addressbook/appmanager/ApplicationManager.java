8337d61d6fdbac3a141576eddec4d347c7411e12
tonio909 <anton.v.alekseev@yandex.ru>
2017-05-19 14:23:38 +0300

    SessionHelper added

--- addressbook-web-tests/src/test/java/lesson2/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/lesson2/addressbook/appmanager/ApplicationManager.java
@@ -11,6 +11,7 @@ public class ApplicationManager {
 
     FirefoxDriver wd;
 
+    private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
 
@@ -29,18 +30,11 @@ public class ApplicationManager {
         wd.get("http://localhost/addressbook/");
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
-        login("admin", "secret");
+        sessionHelper = new SessionHelper(wd);
+        sessionHelper.login("admin", "secret");
     }
 
-    private void login(String username, String password) {
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-    }
+
 
     public void stop() {
         wd.quit();
