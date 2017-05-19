47b5dccdefead7f3c2709ee7513e29a588b98b68
tonio909 <anton.v.alekseev@yandex.ru>
2017-05-19 14:12:48 +0300

    NavigationHelper added

--- addressbook-web-tests/src/test/java/lesson2/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/lesson2/addressbook/appmanager/ApplicationManager.java
@@ -11,6 +11,7 @@ public class ApplicationManager {
 
     FirefoxDriver wd;
 
+    private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
 
     public static boolean isAlertPresent(FirefoxDriver wd) {
@@ -27,6 +28,7 @@ public class ApplicationManager {
         wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         groupHelper = new GroupHelper(wd);
+        navigationHelper = new NavigationHelper(wd);
         login("admin", "secret");
     }
 
@@ -40,15 +42,15 @@ public class ApplicationManager {
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
     }
 
-    public void gotoGroupPage() {
-        wd.findElement(By.linkText("groups")).click();
-    }
-
     public void stop() {
         wd.quit();
     }
 
     public GroupHelper getGroupHelper() {
         return groupHelper;
+    }
+
+    public NavigationHelper getNavigationHelper() {
+        return navigationHelper;
     }
 }
---
+++ addressbook-web-tests/src/test/java/lesson2/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,18 @@
+package lesson2.addressbook.appmanager;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class NavigationHelper {
+    private FirefoxDriver wd;
+
+
+    public NavigationHelper(FirefoxDriver wd) {
+        this.wd = wd;
+    }
+
+    public void gotoGroupPage() {
+        wd.findElement(By.linkText("groups")).click();
+    }
+}
