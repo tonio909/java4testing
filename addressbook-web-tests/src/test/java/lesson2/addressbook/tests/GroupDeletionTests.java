d382f09bceba3bb66ec03896a5e0240b5775625a
tonio909 <anton.v.alekseev@yandex.ru>
2017-05-28 17:20:17 +0300

    Preconditions added

--- addressbook-web-tests/src/test/java/lesson2/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/lesson2/addressbook/tests/GroupDeletionTests.java
@@ -1,5 +1,6 @@
 package lesson2.addressbook.tests;
 
+import lesson2.addressbook.model.GroupData;
 import org.testng.annotations.Test;
 
 public class GroupDeletionTests extends TestBase {
@@ -7,6 +8,11 @@ public class GroupDeletionTests extends TestBase {
     @Test
     public void testGroupDeletion() {
         app.getNavigationHelper().gotoGroupPage();
+
+        if (! app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+        }
+
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
