fc0b29048910b0c78424edbf50ee3dc1e5abfbe7
tonio909 <anton.v.alekseev@yandex.ru>
2017-06-04 16:47:38 +0300

    Added

--- addressbook-web-tests/src/test/java/lesson2/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/lesson2/addressbook/tests/ContactModificationTests.java
@@ -2,7 +2,11 @@ package lesson2.addressbook.tests;
 
 import lesson2.addressbook.model.ContactData;
 import lesson2.addressbook.model.GroupData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class ContactModificationTests extends TestBase {
 
@@ -17,18 +21,36 @@ public class ContactModificationTests extends TestBase {
         }
 
         //Проверяем есть ли контакт для модификации
-        app.getContactHelper().gotoContactPage();
+        app.getNavigationHelper().gotoHomePage();
 
         if (! app.getContactHelper().isThereAContact()) {
+            app.getContactHelper().gotoAddContactFormPage();
             app.getContactHelper().createContact(new ContactData("Anton", "Alekseev", "SPb", "+79119004004", "anton.v.alekseev@yandex.ru", "Group name"));
         }
 
         //Редактируем контакт
+        app.getNavigationHelper().gotoHomePage();
+
+        List<ContactData> before = app.getContactHelper().getContactList();
+
         app.getContactHelper().selectContact();
         app.getContactHelper().editContact();
-        app.getContactHelper().fillContactForm(new ContactData("Anton(edited)", "Alekseev(edited)", "SPb(edited)", "+79119004004(edited)", "anton.v.alekseev@yandex.ru(edited)", null), false);
+        ContactData contact = new ContactData(before.get(before.size() - 1).getId(), "Anton(edited)", "Alekseev(edited)", "SPb(edited)", "+79119004004(edited)", "anton.v.alekseev@yandex.ru(edited)", null);
+        app.getContactHelper().fillContactForm(contact, false);
         app.getContactHelper().submitContactModification();
         app.getContactHelper().returnToHomepage();
+
+        List<ContactData> after = app.getContactHelper().getContactList();
+
+        Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 1);
+        before.add(contact);
+
+        Comparator<? super ContactData> byId = (contact1, contact2) -> Integer.compare(contact1.getId(), contact2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
 
 }
