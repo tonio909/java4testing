7e51e033189e6dfd74c2ea4dd569339c4008ad1d
tonio909 <anton.v.alekseev@yandex.ru>
2017-06-08 16:00:58 +0300

    BeforeSuite / AfterSuite added

--- addressbook-web-tests/src/test/java/lesson2/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/lesson2/addressbook/tests/TestBase.java
@@ -2,19 +2,19 @@ package lesson2.addressbook.tests;
 
 import lesson2.addressbook.appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
-    @BeforeMethod
+    @BeforeSuite
     public void setUp() throws Exception {
         app.init();
     }
 
-    @AfterMethod
+    @AfterSuite
     public void tearDown() {
         app.stop();
     }
