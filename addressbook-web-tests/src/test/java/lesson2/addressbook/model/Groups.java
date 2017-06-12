01d0f7200511944217be6833a8b510b6c4b731a3
tonio909 <anton.v.alekseev@yandex.ru>
2017-06-12 11:08:47 +0300

    Ref

--- addressbook-web-tests/src/test/java/lesson2/addressbook/model/Groups.java
+++ addressbook-web-tests/src/test/java/lesson2/addressbook/model/Groups.java
@@ -5,7 +5,6 @@ import com.google.common.collect.ForwardingSet;
 import java.util.HashSet;
 import java.util.Set;
 
-
 public class Groups extends ForwardingSet<GroupData>{
 
 private Set<GroupData> delegate;
@@ -15,22 +14,21 @@ private Set<GroupData> delegate;
     }
 
     public Groups() {
-        this.delegate = new HashSet<>();
+        this.delegate = new HashSet<GroupData>();
     }
-
 
     @Override
     protected Set<GroupData> delegate() {
         return delegate;
     }
 
-    public  Groups withAdded(GroupData group) {
+    public Groups withAdded(GroupData group) {
         Groups groups = new Groups(this);
         groups.add(group);
         return groups;
     }
 
-    public  Groups withoutAdded(GroupData group) {
+    public Groups withoutAdded(GroupData group) {
         Groups groups = new Groups(this);
         groups.remove(group);
         return groups;
