cf11bfa511cd532736dfcf039914c86c61a5bade
tonio909 <anton.v.alekseev@yandex.ru>
2017-06-15 10:31:49 +0300

    Небольшие изменения

--- addressbook-web-tests/src/test/java/lesson2/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/lesson2/addressbook/appmanager/ContactHelper.java
@@ -17,7 +17,7 @@ public class ContactHelper extends HelperBase {
     }
 
     public void returntoHomePage() {
-        click(By.linkText("home page"));
+        click(By.linkText("home"));
     }
 
     public void submitContactCreation() {
--- addressbook-web-tests/src/test/java/lesson2/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/lesson2/addressbook/tests/ContactDeletionTests.java
@@ -37,7 +37,6 @@ public class ContactDeletionTests extends TestBase {
         Contacts before = app.contact().all();
         ContactData deletedContact = before.iterator().next();
         app.contact().delete(deletedContact);
-        app.goTo().gotoHomePage();
         assertThat(app.contact().count(), equalTo(before.size() - 1));
         Contacts after = app.contact().all();
         assertThat(after, equalTo(before.without(deletedContact)));
