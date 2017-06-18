1bd71986f209dd7cebcbf69819c8dbe05b5f32b2
tonio909 <anton.v.alekseev@yandex.ru>
2017-06-18 14:11:32 +0300

    Добавлен DataProvider для xml и json для тестов создания контактов

--- addressbook-web-tests/src/test/java/lesson2/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/lesson2/addressbook/tests/ContactCreationTests.java
@@ -1,17 +1,56 @@
 package lesson2.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.common.reflect.TypeToken;
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import lesson2.addressbook.model.ContactData;
 import lesson2.addressbook.model.Contacts;
 import lesson2.addressbook.model.GroupData;
 
+import java.io.BufferedReader;
 import java.io.File;
-
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.io.FileReader;
+import java.util.stream.Collectors;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
+
+    @DataProvider
+    public Iterator<Object[]> validContactsFromXml() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+        String xml = "";
+        String line = reader.readLine();
+        while (line != null) {
+            xml += line;
+            line = reader.readLine();
+        }
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+        return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
+    }
+
+    @DataProvider
+    public Iterator<Object[]> validContactsFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null) {
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
+        return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
+    }
+
 
     @BeforeMethod
     //Проверяем существует ли группа для добавления контакта в следующем шаге
